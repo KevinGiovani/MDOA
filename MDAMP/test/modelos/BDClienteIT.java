54f202977e2a82008c2ae4ffe73eb82c2079bcff
JoelDLC <delacruz.joel@uabc.edu.mx>
2021-04-30 15:24:52 -0700

    - Pruebas Unitarias (Ordenadas)

--- MDAMP/test/modelos/BDClienteIT.java
+++ MDAMP/test/modelos/BDClienteIT.java
@@ -1,8 +1,11 @@
-/**
- * Prueba Unitaria aplicada al modelo 'BDCliente' 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package modelos;
 
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,39 +15,40 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Inzunza Kevin
- * @version 17-04-2021
+ * @author kevin
  */
-public class BDClienteTest {
-    
-    public BDClienteTest() {
+public class BDClienteIT {
+
+    public BDClienteIT() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
      * Test of agregarDatos method, of class BDCliente.
+     *
+     * @throws java.lang.Exception
      */
     @Test
     public void testAgregarDatos() throws Exception {
         System.out.println("agregarDatos");
-        Cliente nCliente = new Cliente("Test","Test","33123421","MiPolloTest");
+        Cliente nCliente = new Cliente("Test", "Test", "433421", "MiPolloTest");
         BDCliente instance = new BDCliente();
-        int tel=33123421;
+        int tel = 433421;
         instance.agregarDatos(nCliente);
         Cliente result = instance.buscar(tel);
         assertEquals(nCliente.getNombre(), result.getNombre());
@@ -52,15 +56,28 @@ public class BDClienteTest {
 
     /**
      * Test of buscar method, of class BDCliente.
+     *
+     * @throws java.lang.Exception
      */
     @Test
     public void testBuscar() throws Exception {
         System.out.println("buscar");
         int tel = 1;
         BDCliente instance = new BDCliente();
-        Cliente expResult = new Cliente("Cliente","Cliente","1","MiPollo");
         Cliente result = instance.buscar(tel);
-        assertEquals(expResult.getNombre(), result.getNombre());
+        assertNotNull(result.getNombre());
     }
-    
+
+    /**
+     * Test of consultar method, of class BDCliente.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testConsultar() throws Exception {
+        System.out.println("consultar");
+        BDCliente instance = new BDCliente();
+        ArrayList<Cliente> result = instance.consultar();
+        assertTrue(!result.isEmpty());
+    }
 }
